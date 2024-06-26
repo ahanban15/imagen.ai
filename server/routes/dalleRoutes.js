678e2792fbc79694e121b76978f92da72891cbdd
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2024-06-26 08:17:16 +0530

    completion commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,38 +1,71 @@
+// import express from 'express';
+// import * as dotenv from 'dotenv';
+// import OpenAI from 'openai';
+
+// dotenv.config();
+
+// const router = express.Router();
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+// });
+
+// router.route('/').get((req, res) => {
+//   res.status(200).json({ message: 'Hello from DALL-E!' });
+// });
+
+// router.route('/').post(async (req, res) => {
+//   try {
+//     const { prompt } = req.body;
+
+//     const aiResponse = await openai.createImage({
+//       prompt,
+//       n: 1,
+//       size: '1024x1024',
+//       response_format: 'b64_json',
+//     });
+
+//     const image = aiResponse.data.data[0].b64_json;
+//     res.status(200).json({ photo: image });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+//   }
+// });
+
+// export default router;
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
-
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'Hello from DALL-E!' });
+res.send('Hello AI Image Generation App!' );
 });
 
 router.route('/').post(async (req, res) => {
-  try {
-    const { prompt } = req.body;
+try {
+const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
-      prompt,
-      n: 1,
-      size: '1024x1024',
-      response_format: 'b64_json',
-    });
-
-    const image = aiResponse.data.data[0].b64_json;
-    res.status(200).json({ photo: image });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
-  }
+const aiResponse = await openai.images.generate({
+  prompt,
+  n: 1,
+  size: '1024x1024',
+  response_format: 'b64_json',
 });
 
+const image = aiResponse.data[0].b64_json;
+res.status(200).json({ photo: image });
+} catch (error) {
+console.error(error);
+res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+}
+});
 export default router;
